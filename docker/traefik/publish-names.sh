e60a50ffc592ea866511d7b2813ea98efdc8730e
Aleksandr Barmin <abarmin@outlook.com>
2025-01-26 16:37:30 +0000

    Update after miving behind Microtik

--- docker/traefik/publish-names.sh
+++ docker/traefik/publish-names.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/avahi-publish -a -R portainer.raspberrypi.local 192.168.1.183 &
-/usr/bin/avahi-publish -a -R glances.raspberrypi.local 192.168.1.183 &
+/usr/bin/avahi-publish -a -R portainer.raspberrypi.local 192.168.88.65 &
+/usr/bin/avahi-publish -a -R glances.raspberrypi.local 192.168.88.65 &
