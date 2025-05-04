f566d3eff774f9c6b408aeaf44cdfaec41766c7b
Aleksandr Barmin <abarmin@outlook.com>
2025-05-04 11:57:54 +0100

    Add mantis

---
+++ ansible/roles/bt-abarmin-pro/dist/config/custom_constants_inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ABarmin
+ * Date: 24.11.2014
+ * Time: 14:32
+ */
+    define("ASSESSMENT", 60);
+    define("REVIEW", 61);
+    define("ALLOCATION", 62);
+    define("EXECUTION", 63);
+    define("QUALIFICATION", 64);
+    define("TESTING", 65);
+    define("COMPLETED", 66);
