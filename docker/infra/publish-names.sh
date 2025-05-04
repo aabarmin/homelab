4f4bf066f6ee0df0105b50915be5cc1dddc7ce12
Aleksandr Barmin <abarmin@outlook.com>
2025-05-04 12:01:10 +0100

    Mantis is now just a simple php project running on Apache

--- docker/infra/publish-names.sh
+++ docker/infra/publish-names.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-/usr/bin/avahi-publish -a -R portainer.raspberrypi.local 192.168.1.183 &
-/usr/bin/avahi-publish -a -R glances.raspberrypi.local 192.168.1.183 &
 /usr/bin/avahi-publish -a -R pma.raspberrypi.local 192.168.1.183 &
