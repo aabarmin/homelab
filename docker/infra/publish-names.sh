e1239a77b52e7b0faadb3fbc6edbcb5d6f18b0a8
Aleksandr Barmin <abarmin@outlook.com>
2025-05-04 12:43:10 +0100

    Remove some unnecessary docker configs

--- docker/infra/publish-names.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/avahi-publish -a -R pma.raspberrypi.local 192.168.1.183 &
--- docker/traefik/publish-names.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/bin/avahi-publish -a -R portainer.raspberrypi.local 192.168.88.65 &
-/usr/bin/avahi-publish -a -R glances.raspberrypi.local 192.168.88.65 &
