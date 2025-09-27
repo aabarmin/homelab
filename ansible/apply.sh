82eed984496b6af5beaba18dad0a8daa6939097a
Aleksandr Barmin <abarmin@outlook.com>
2025-09-27 20:05:20 +0100

    Remove check of a cloudflare token - not needed anymore

--- ansible/apply.sh
+++ ansible/apply.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # git pull
-if [ -z "${CLOUDFLARE_TOKEN}" ]; then
-    echo "CLOUDFLARE_TOKEN environment variable not set"
-    exit 1
-fi
+# Cloudflare is disabled so token is not needed
+# if [ -z "${CLOUDFLARE_TOKEN}" ]; then
+#     echo "CLOUDFLARE_TOKEN environment variable not set"
+#     exit 1
+# fi
 
-ansible-playbook -i inventory.yml playbook.yml -e "CLOUDFLARE_TOKEN=${CLOUDFLARE_TOKEN}"
+# ansible-playbook -i inventory.yml playbook.yml -e "CLOUDFLARE_TOKEN=${CLOUDFLARE_TOKEN}"
+ansible-playbook -i inventory.yml playbook.yml
