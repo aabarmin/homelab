88f7524ac7cd5316e5a209c2922d207df7ba8bc6
Aleksandr Barmin <abarmin@outlook.com>
2026-04-19 16:20:31 +0100

    Migrated to cloudflared

--- ansible/apply.sh
+++ ansible/apply.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-# git pull
-# Cloudflare is disabled so token is not needed
-# if [ -z "${CLOUDFLARE_TOKEN}" ]; then
-#     echo "CLOUDFLARE_TOKEN environment variable not set"
-#     exit 1
-# fi
-
-# ansible-playbook -i inventory.yml playbook.yml -e "CLOUDFLARE_TOKEN=${CLOUDFLARE_TOKEN}"
-ansible-playbook -i inventory.yml playbook.yml -K
+ansible-playbook -i inventory.yml playbook.yml -K --ask-vault-pass
